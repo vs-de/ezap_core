6affcefc2c9d3b50f35d37483d3cb28b02d44177
vs <valentin.schulte@wecuddle.de>
2013-06-15 15:19:58 +0200

    ezap has gem opts addition

--- lib/ezap/base.rb
+++ lib/ezap/base.rb
@@ -6,4 +6,15 @@ module Ezap
     end
 
   end
+
+  def self.redis_gem_opts
+    cfg = Ezap::Service::GlobalMaster.config(:opts, :cache_storage)
+    addr = cfg['addr'].split(':') rescue nil
+    unless addr
+      $stderr.puts "warning: redis conf not given, falling back to empty conf(/local)" 
+      return {}
+    end
+    {host: addr[0], port: addr[1]}
+  end
+
 end
