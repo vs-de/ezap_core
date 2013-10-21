50d12b901173a17da39a2f00807d9384b1741236
vs <valentin@vs-coding.de>
2013-10-21 12:25:55 +0200

    license change to GPLv3

--- lib/ezap/config_access.rb
+++ lib/ezap/config_access.rb
@@ -1,3 +1,12 @@
+#####
+# Copyright 2013, Valentin Schulte
+# This file is part of Ezap.
+# Ezap is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3 
+# as published by the Free Software Foundation.
+# You should have received a copy of the GNU General Public License
+# in the file COPYING along with Ezap. If not, see <http://www.gnu.org/licenses/>.
+#####
 
 module Ezap::ConfigAccess
   
--- lib/ruby_ext/string_ext.rb
+++ lib/ruby_ext/string_ext.rb
@@ -1,3 +1,12 @@
+#####
+# Copyright 2013, Valentin Schulte
+# This file is part of Ezap.
+# Ezap is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3 
+# as published by the Free Software Foundation.
+# You should have received a copy of the GNU General Public License
+# in the file COPYING along with Ezap. If not, see <http://www.gnu.org/licenses/>.
+#####
 class String
   #TODO: change capitalize with sth that only changes the first letter up, not also others down
   #and all other non-alphas should act as sep, too
